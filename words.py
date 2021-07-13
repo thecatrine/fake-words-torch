42fa213ddb5271a6ec5c460473ad34d8aef098e0
Catherine David <miscatulated@gmail.com>
2021-07-13 02:09:15 -0700

    With batches

--- words.py
+++ words.py
@@ -14,37 +14,6 @@ words = []
 for temp in temp_words:
     words = words + [temp[:i] for i in range(1, len(temp)+1)]
 
-#embeds = torch.nn.Embedding(26, 50)
-
-#vec = torch.tensor([letter_to_idx[x] for x in words[0]], dtype=torch.long)
-
-
-class Test(torch.nn.Module):
-
-    def __init__(self, embedding_dim, context_size):
-        super(Test, self).__init__()
-        self.embeddings = torch.nn.Embedding(27, embedding_dim)
-        self.linear1 = torch.nn.Linear(context_size * embedding_dim, 128)
-        self.linear2 = torch.nn.Linear(128, 27)
-
-    def forward(self, inputs):
-        embeds = self.embeddings(inputs).view((1, -1))
-
-        out = self.linear1(embeds)
-        out = torch.nn.functional.relu(out)
-        out = self.linear2(out)
-
-        log_probs = torch.nn.functional.log_softmax(out, dim=1)
-        return log_probs
-
-
-losses = []
-loss_function = torch.nn.NLLLoss()
-EMBEDDING_DIM = 50
-CONTEXT_SIZE = 20
-model = Test(EMBEDDING_DIM, CONTEXT_SIZE)
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
 
 def letter_to_vec(letter):
     return torch.tensor(
@@ -69,39 +38,86 @@ def word_to_vec(in_word):
     return vec
 
 
+BATCH_SIZE = 1
+train_batches = []
+test_batches = []
+
+for i in range(0, math.floor(len(words) / BATCH_SIZE), BATCH_SIZE):
+    train_batch = []
+    test_batch = []
+    for w in words[i:i + BATCH_SIZE]:
+        train_batch.append(word_to_vec(w[:-1]))
+        test_batch.append(letter_to_vec(w[-1]))
+
+    train_batches.append(torch.stack(train_batch))
+    test_batches.append(torch.stack(test_batch))
+    
+#embeds = torch.nn.Embedding(26, 50)
+
+#vec = torch.tensor([letter_to_idx[x] for x in words[0]], dtype=torch.long)
+
+
+class Test(torch.nn.Module):
+
+    def __init__(self, embedding_dim, context_size):
+        super(Test, self).__init__()
+        self.embeddings = torch.nn.Embedding(27, embedding_dim, device=device)
+        self.linear1 = torch.nn.Linear(context_size * embedding_dim, 128, device=device)
+        self.linear2 = torch.nn.Linear(128, 27, device=device)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs).view(BATCH_SIZE, 1, -1)
+
+        out = self.linear1(embeds)
+        out = torch.nn.functional.relu(out)
+        out = self.linear2(out)
+
+        log_probs = torch.nn.functional.log_softmax(out, dim=2)
+        return log_probs
+
+
+losses = []
+loss_function = torch.nn.NLLLoss()
+EMBEDDING_DIM = 50
+CONTEXT_SIZE = 20
+model = Test(EMBEDDING_DIM, CONTEXT_SIZE)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
 def blarg(a):
     for i in range(0, 5):
         vec = word_to_vec(a)
-        nxt = torch.argmax(model(vec))
+        out = model(torch.stack([vec for x in range(0, BATCH_SIZE)]))[0][0]
+        nxt = torch.argmax(out)
         a = a + letters[nxt]
 
     return a
 
+def main():
+    for epoch in range(10):
+        print(f"Epoch {epoch}")
+        total_loss = 0
+        i = 0
+        for j in range(0, len(train_batches)):
+            i += 1
+            if i % 999 == 0:
+                print(i)
 
-for epoch in range(10):
-    print(f"Epoch {epoch}")
-    total_loss = 0
-    i = 0
-    for word in words:
-        i += 1
-        if i % 999 == 0:
-            print(i)
+            # RSI
 
-        ww = word_to_vec(word[:-1])
-        test_ww = letter_to_vec(word[-1])
+            model.zero_grad()
+            log_probs = model(train_batches[j])
+            loss = loss_function(log_probs.view(-1, 27, BATCH_SIZE), test_batches[j].view(-1, BATCH_SIZE))
 
-        model.zero_grad()
-        log_probs = model(ww)
-        loss = loss_function(
-            log_probs,
-            test_ww,
-        )
+            loss.backward()
+            optimizer.step()
 
-        loss.backward()
-        optimizer.step()
+            total_loss += loss.item()
+        losses.append(total_loss)
 
-        total_loss += loss.item()
-    losses.append(total_loss)
+    print(losses)
+    import pdb; pdb.set_trace()
 
-print(losses)
-import pdb;pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
